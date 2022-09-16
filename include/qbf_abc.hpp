f8533e9a99edeeb70323c805fb10044959d9d1c0
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-09-16 13:15:57 -0700

    remove redundant func

--- include/qbf_abc.hpp
+++ include/qbf_abc.hpp
@@ -3,5 +3,3 @@
 #include <string>
 
 int SolveQBF(std::string specname, std::string implname, std::string outname, std::string prefix = "_", bool fVerbose = false, bool fVerbose2 = false);
-
-int SolveQBFRelation(std::string specname, std::string implname, std::string outname, std::string prefix = "_", bool fVerbose = false, bool fVerbose2 = false);
